3c1b3b848e715aa8d6e4075031116a428e59b311
Moto Ishizawa <summerwind.jp@gmail.com>
2012-04-20 14:20:54 +0900

    Fix design

--- ideapocket/fuel/app/views/front/index.php
+++ ideapocket/fuel/app/views/front/index.php
@@ -1,98 +1,33 @@
 <div class="issue">
 <h2>Issue No.1</h2>
 <p>You can't connect the dots looking forward; you can only connect them looking backwards. So you have to trust that the dots will somehow connect in your future. You have to trust in something — your gut, destiny, life, karma, whatever. This approach has never let me down, and it has made all the difference in my life.</p>
-<p><a href="#">Like</a> - <a href="#">Solution</a></p>
-<div>
-
-<input type="text" placeholder="Solution" class="span8"><br/>
-<textarea placeholder="Description" class="span4"></textarea>
-<p><button class="btn" type="submit">Submit</button></p>
-</div>
+<p>by htakei - <a href="#">Like (21)</a> - <a href="#">Solution (5)</a> - <a href="#">See more</a></p>
+<form class="well">
+    <input type="text" placeholder="Solution" class="span8"><br>
+    <textarea placeholder="Description" class="span8"></textarea><br>
+    <button class="btn" type="submit">Submit</button>
+</form>
 <dl class="solution">
-    <dt>Solution 1</dt>
-    <!--<dd>Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma — which is living with the results of other people’s thinking.</dd>-->
-    <dd class="info"><a href="#">Like</a> - <a href="#">Comment</a></p>
-    <dt>Solution 2</dt>
-    <!--<dd>Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma — which is living with the results of other people’s thinking.</dd>-->
-    <dd class="info"><a href="#">Like</a> - <a href="#">Comment</a></p>
-    <dt>Solution 3</dt>
-    <!--<dd>Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma — which is living with the results of other people’s thinking.</dd>-->
-    <dd class="info"><a href="#">Like</a> - <a href="#">Comment</a></p>
+    <dt>This is a title of solution, Wohooooooooooooooooooooooooooooooo!!</dt>
+    <dd class="info">by mishizaw - <a href="#">Like (2)</a></p>
+    <dt>This is a title of solution, Wohooooooooooooooooooooooooooooooo!!</dt>
+    <dd class="info">by mishizaw - <a href="#">Like (2)</a></p>
+    <dt>This is a title of solution, Wohooooooooooooooooooooooooooooooo!!</dt>
+    <dd class="info">by mishizaw - <a href="#">Like (2)</a></p>
 </dl>
 </div>
 
 <div class="issue">
 <h2>Issue No.2</h2>
 <p>You can't connect the dots looking forward; you can only connect them looking backwards. So you have to trust that the dots will somehow connect in your future. You have to trust in something — your gut, destiny, life, karma, whatever. This approach has never let me down, and it has made all the difference in my life.</p>
-<p><a href="#">Like</a> - <a href="#">Solution</a></p>
+<p>by htakei - <a href="#">Like (21)</a> - <a href="#">Solution (5)</a> - <a href="#">See more</a></p>
 <dl class="solution">
-    <dt>Solution 1</dt>
-    <!--<dd>Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma — which is living with the results of other people’s thinking.</dd>-->
-    <dd class="info"><a href="#">Like</a> - <a href="#">Comment</a></p>
-    <dt>Solution 2</dt>
-    <!--<dd>Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma — which is living with the results of other people’s thinking.</dd>-->
-    <dd class="info"><a href="#">Like</a> - <a href="#">Comment</a></p>
-    <dt>Solution 3</dt>
-    <!--<dd>Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma — which is living with the results of other people’s thinking.</dd>-->
-    <dd class="info"><a href="#">Like</a> - <a href="#">Comment</a></p>
+    <dt>This is a title of solution, Wohooooooooooooooooooooooooooooooo!!</dt>
+    <dd class="info">by mishizaw - <a href="#">Like (2)</a></p>
+    <dt>This is a title of solution, Wohooooooooooooooooooooooooooooooo!!</dt>
+    <dd class="info">by mishizaw - <a href="#">Like (2)</a></p>
+    <dt>This is a title of solution, Wohooooooooooooooooooooooooooooooo!!</dt>
+    <dd class="info">by mishizaw - <a href="#">Like (2)</a></p>
 </dl>
 </div>
 
-<style>
-.issue {
-    padding: 0 0 30px 0;
-}
-
-.issue h2 {
-    position: relative;
-}
-
-.issue h2 a {
-    position: absolute;
-    right: 0;
-    font-size: 12px;
-    color: white;
-    background-color: #0069d6;
-    line-height: 1;
-    padding: 7px;
-}
-
-.solution {
-    border-bottom: 1px solid #ccc;
-    list-style: none;
-}
-
-.solution dt {
-    padding: 8px 0 5px 0;
-    position: relative;
-    border-top: 1px solid #ccc;
-}
-
-.solution dt a {
-    position: absolute;
-    right: 0;
-    font-size: 12px;
-    color: white;
-    background-color: #0069d6;
-    line-height: 1;
-    padding: 4px;
-}
-
-.solution dd {
-    margin: 0;
-}
-
-.solution dd.info {
-    padding: 5px 0 0 0;
-}
-
-.solution dd ul {
-    margin: 0;
-    list-style: none;
-}
-
-.solution dd ul li {
-    padding: 2px 0 0;
-    border-bottom: 1px solid #ccc;
-}
-</style>
--- ideapocket/fuel/app/views/template.php
+++ ideapocket/fuel/app/views/template.php
@@ -9,10 +9,64 @@
       padding: 20px 40px;
       border-top: 8px solid #000;
     }
+    
     #content {
       margin: 20px 0 0; 
     }
-	</style>
+
+    .issue {
+        padding: 0 0 30px 0;
+    }
+
+    .issue h2 {
+        position: relative;
+    }
+
+    .issue h2 a {
+        position: absolute;
+        right: 0;
+        font-size: 12px;
+        color: white;
+        background-color: #0069d6;
+        line-height: 1;
+        padding: 7px;
+    }
+
+    .solution {
+        border-bottom: 1px solid #ccc;
+        list-style: none;
+    }
+
+    .solution dt {
+        padding: 8px 0 2px 0;
+        position: relative;
+        border-top: 1px solid #ccc;
+    }
+
+    .solution dt a {
+        position: absolute;
+        right: 0;
+        font-size: 12px;
+        color: white;
+        background-color: #0069d6;
+        line-height: 1;
+        padding: 4px;
+    }
+
+    .solution dd {
+        margin: 0;
+    }
+
+    .solution dd ul {
+        margin: 0;
+        list-style: none;
+    }
+
+    .solution dd ul li {
+        padding: 2px 0 0;
+        border-bottom: 1px solid #ccc;
+    }
+    </style>
 </head>
 
 <body>
@@ -25,8 +79,11 @@
           <form>
             <input type="text" class="span4" placeholder="Issue">
             <p><textarea class="span4" rows="15" placeholder="Description"></textarea></p>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn">Submit</button>
           </form>
+          
+          <h2>いいね数ランキング</h2>
+          <h2>ソリューション数ランキング</h2>
         </div>
         <div class="span9"><?php echo $content ?></div>
       </div>
