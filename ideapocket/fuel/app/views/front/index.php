07744be7ac5a84a0600d3766bd3fa8ce43e3b53f
Moto Ishizawa <summerwind.jp@gmail.com>
2012-04-20 15:41:33 +0900

    Fix design

--- ideapocket/fuel/app/views/front/index.php
+++ ideapocket/fuel/app/views/front/index.php
@@ -31,3 +31,17 @@
 </dl>
 </div>
 
+<div class="issue">
+<h2>Issue No.3</h2>
+<p>You can't connect the dots looking forward; you can only connect them looking backwards. So you have to trust that the dots will somehow connect in your future. You have to trust in something — your gut, destiny, life, karma, whatever. This approach has never let me down, and it has made all the difference in my life.</p>
+<p>by htakei - <a href="#">Like (21)</a> - <a href="#">Solution (5)</a> - <a href="#">See more</a></p>
+<dl class="solution">
+    <dt>This is a title of solution, Wohooooooooooooooooooooooooooooooo!!</dt>
+    <dd class="info">by mishizaw - <a href="#">Like (2)</a></p>
+    <dt>This is a title of solution, Wohooooooooooooooooooooooooooooooo!!</dt>
+    <dd class="info">by mishizaw - <a href="#">Like (2)</a></p>
+    <dt>This is a title of solution, Wohooooooooooooooooooooooooooooooo!!</dt>
+    <dd class="info">by mishizaw - <a href="#">Like (2)</a></p>
+</dl>
+</div>
+
--- ideapocket/fuel/app/views/template.php
+++ ideapocket/fuel/app/views/template.php
@@ -4,10 +4,19 @@
 	<meta charset="utf-8">
 	<title><?php echo $title; ?></title>
 	<?php echo Asset::css('bootstrap.css'); ?>
+	<link href='http://fonts.googleapis.com/css?family=Fredoka+One' rel='stylesheet' type='text/css'>
 	<style>
     body {
       padding: 20px 40px;
       border-top: 8px solid #000;
+    }
+    
+    header {
+        padding: 0 0 20px 0;
+    }
+    
+    h1 {
+        font-family: 'Fredoka One', sans-serif;
     }
     
     #content {
@@ -66,35 +75,83 @@
         padding: 2px 0 0;
         border-bottom: 1px solid #ccc;
     }
+    
+    .rank {
+        padding-bottom: 20px;
+    }
+    
+    .rank h2 {
+        font-size: 15px;
+        border-bottom: 1px solid #ccc;
+    }
+    
+    .rank ol {
+        margin: 0;
+        list-style: none;
+    }
+    
+    .rank ol li {
+        padding: 5px 0;
+        border-bottom: 1px solid #ccc;
+    }
+    
+    .rank ol li span {
+        width: 20px;
+        margin: 0 5px 0 0;
+        text-align: center;
+        display: inline-block;
+    }
     </style>
 </head>
 
 <body>
-  <div class="row-fluid">
-    <div class="span12">
-      <div id="content" class="row-fluid">
-        <div class="span3">
-          <h1><?php echo $title; ?></h1> 
-          <p>Submit your issue!</p>
-          <form>
-            <input type="text" class="span4" placeholder="Issue">
-            <p><textarea class="span4" rows="15" placeholder="Description"></textarea></p>
-            <button type="submit" class="btn">Submit</button>
-          </form>
+    <div class="row-fluid">
+        <div class="span12">
+            <div id="content" class="row-fluid">
+                <div class="span3">
+                    <header>
+                        <h1><?php echo $title; ?></h1> 
+                        <p>Submit your issue!</p>
+                    </header>
+                    
+                    <form class="well">
+                        <input type="text" placeholder="Issue"><br>
+                        <textarea rows="13" placeholder="Description"></textarea><br>
+                        <button type="submit" class="btn">Submit</button>
+                    </form>
+                    
+                    <div class="rank">
+                        <h2>Like Ranking</h2>
+                        <ol>
+                            <li><span class="badge badge-info">123</span> <a href="#">こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは</a></li>
+                            <li><span class="badge badge-info">18</span> <a href="#">Issue No.2</a></li>
+                            <li><span class="badge badge-info">10</span> <a href="#">Issue No.3</a></li>
+                            <li><span class="badge badge-info">8</span> <a href="#">Issue No.4</a></li>
+                            <li><span class="badge badge-info">3</span> <a href="#">Issue No.5</a></li>
+                        </ol>
+                    </div>
           
-          <h2>いいね数ランキング</h2>
-          <h2>ソリューション数ランキング</h2>
+                    <div class="rank">
+                        <h2>Solution Ranking</h2>
+                        <ol>
+                            <li><span class="badge badge-info">23</span> <a href="#">Issue No.1</a></li>
+                            <li><span class="badge badge-info">18</span> <a href="#">Issue No.2</a></li>
+                            <li><span class="badge badge-info">10</span> <a href="#">Issue No.3</a></li>
+                            <li><span class="badge badge-info">8</span> <a href="#">Issue No.4</a></li>
+                            <li><span class="badge badge-info">3</span> <a href="#">Issue No.5</a></li>
+                        </ol>
+                    </div>
+                </div>
+                <div class="span9"><?php echo $content ?></div>
+            </div>
         </div>
-        <div class="span9"><?php echo $content ?></div>
-      </div>
     </div>
-  </div>
-  
-  <footer>
-    <p>
-      Made by &quot;Team Messy&quot;<br>
-      <small>Version: 0.1b</small>
-    </p>
-  </footer>
+    
+    <footer>
+        <p>
+        Made by &quot;Team Messy&quot;<br>
+        <small>Version: 0.1b</small>
+        </p>
+    </footer>
 </body>
 </html>
