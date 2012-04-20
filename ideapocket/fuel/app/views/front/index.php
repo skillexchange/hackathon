935280ae15c14c6170cdc000497d6743662e7676
Moto Ishizawa <summerwind.jp@gmail.com>
2012-04-21 03:00:28 +0900

    Fix design

--- ideapocket/fuel/app/views/front/index.php
+++ ideapocket/fuel/app/views/front/index.php
@@ -1,47 +1,127 @@
 <div class="issue">
-<h2>Issue No.1</h2>
-<p>You can't connect the dots looking forward; you can only connect them looking backwards. So you have to trust that the dots will somehow connect in your future. You have to trust in something — your gut, destiny, life, karma, whatever. This approach has never let me down, and it has made all the difference in my life.</p>
-<p>by htakei - <a href="#">Like (21)</a> - <a href="#">Solution (5)</a> - <a href="#">See more</a></p>
-<form class="well">
-    <input type="text" placeholder="Solution" class="span8"><br>
-    <textarea placeholder="Description" class="span8"></textarea><br>
-    <button class="btn" type="submit">Submit</button>
-</form>
-<dl class="solution">
-    <dt>This is a title of solution, Wohooooooooooooooooooooooooooooooo!!</dt>
-    <dd class="info">by mishizaw - <a href="#">Like (2)</a></p>
-    <dt>This is a title of solution, Wohooooooooooooooooooooooooooooooo!!</dt>
-    <dd class="info">by mishizaw - <a href="#">Like (2)</a></p>
-    <dt>This is a title of solution, Wohooooooooooooooooooooooooooooooo!!</dt>
-    <dd class="info">by mishizaw - <a href="#">Like (2)</a></p>
-</dl>
+    <h2>Stay foolish, Stay hungry.</h2>
+    <p>私が若い頃 "The Whole Earth Catalogue 全地球カタログ" というすごい出版物があって、私と同じ世代ではバイブルのように扱われていました。それはステュアート・ブランドという人が、ここからそれほど遠くないメンローパークで制作したもので、彼の詩的なタッチで彩られていました。１９６０年代の終わり頃はパソコンもDTPもない時代ですから、全てタイプライターとハサミとポラロイドカメラで作られていました。それはまるでグーグルのペーパーバック版のようなもので、グーグルが３５年遡って登場したかのような理想的な本で、すごいツールと壮大な概念に溢れかえっていました。</p>
+    <p>スチュアートと彼のチームは ”The Whole Earth Catalogue” を何度か発行しましたが、ひと通りの内容を網羅した時点で最終号を出しました。それは１９７０年代半ばで、私がちょうど君たちの年代だった頃です。最終号の裏表紙は、朝早い田舎道の写真だったのですが、それはヒッチハイクの経験があればどこか見たことある光景でした。写真の下には "Stay hungry, Stay foolish." という言葉が書かれていたのです。</p>
+    
+    <ul class="info">
+        <li><i class="icon-user"></i> <a href="#">takewick</a></li>
+        <li><i class="icon-heart"></i> <a href="#">いいね！</a></li>
+        <li><i class="icon-inbox"></i> <a href="#">課題の解決方法を投稿</a></li>
+        <li><i class="icon-info-sign"></i> <a href="#">もっと詳しく</a></li>
+    </ul>
+    
+    <div class="solution-form">
+        <h3>課題の解決方法</h3>
+        <p>この課題に対する<strong>解決方法</strong>を投稿してください。</p>
+        <form class="well">
+            <p><input type="text" placeholder="タイトル" class="span6"></p>
+            <p><textarea placeholder="具体的な解決方法" rows="5"></textarea></p>
+            <p><button class="btn" type="submit">投稿</button></p>
+        </form>
+    </div>
+        
+    <div class="solution">
+        <dl>
+            <dt>自分の根性、運命、人生、カルマ、何でもいいから、とにかく信じるのです。歩む道のどこかで点と点がつながると信じれば、自信を持って思うままに生きることができます。たとえ人と違う道を歩んでも、信じることが全てを変えてくれるのです。</dt>
+            <dd>
+                <ul class="info">
+                    <li><i class="icon-user"></i> <a href="#">summerwind</a></li>
+                    <li><i class="icon-heart"></i> <a href="#">いいね！</a></li>
+                </ul>
+            </dd>
+            <dt>未来に先回りして点と点をつなげることはできない。君たちにできるのは過去を振り返ってつなげることだけなんだ。だから点と点がいつか何らかのかたちでつながると信じなければならない。</dt>
+            <dd>
+                <ul class="info">
+                    <li><i class="icon-user"></i> <a href="#">summerwind</a></li>
+                    <li><i class="icon-heart"></i> <a href="#">いいね！</a></li>
+                </ul>
+            </dd>
+            <dt>Steve Jobs、なんで死んでしもうたんや。</dt>
+            <dd>
+                <ul class="info">
+                    <li><i class="icon-user"></i> <a href="#">summerwind</a></li>
+                    <li><i class="icon-heart"></i> <a href="#">いいね！</a></li>
+                </ul>
+            </dd>
+        </dl>
+    </div>
 </div>
 
 <div class="issue">
-<h2>Issue No.2</h2>
-<p>You can't connect the dots looking forward; you can only connect them looking backwards. So you have to trust that the dots will somehow connect in your future. You have to trust in something — your gut, destiny, life, karma, whatever. This approach has never let me down, and it has made all the difference in my life.</p>
-<p>by htakei - <a href="#">Like (21)</a> - <a href="#">Solution (5)</a> - <a href="#">See more</a></p>
-<dl class="solution">
-    <dt>This is a title of solution, Wohooooooooooooooooooooooooooooooo!!</dt>
-    <dd class="info">by mishizaw - <a href="#">Like (2)</a></p>
-    <dt>This is a title of solution, Wohooooooooooooooooooooooooooooooo!!</dt>
-    <dd class="info">by mishizaw - <a href="#">Like (2)</a></p>
-    <dt>This is a title of solution, Wohooooooooooooooooooooooooooooooo!!</dt>
-    <dd class="info">by mishizaw - <a href="#">Like (2)</a></p>
-</dl>
+    <h2>Stay foolish, Stay hungry.</h2>
+    <p>私が若い頃 "The Whole Earth Catalogue 全地球カタログ" というすごい出版物があって、私と同じ世代ではバイブルのように扱われていました。それはステュアート・ブランドという人が、ここからそれほど遠くないメンローパークで制作したもので、彼の詩的なタッチで彩られていました。１９６０年代の終わり頃はパソコンもDTPもない時代ですから、全てタイプライターとハサミとポラロイドカメラで作られていました。それはまるでグーグルのペーパーバック版のようなもので、グーグルが３５年遡って登場したかのような理想的な本で、すごいツールと壮大な概念に溢れかえっていました。</p>
+    <p>スチュアートと彼のチームは ”The Whole Earth Catalogue” を何度か発行しましたが、ひと通りの内容を網羅した時点で最終号を出しました。それは１９７０年代半ばで、私がちょうど君たちの年代だった頃です。最終号の裏表紙は、朝早い田舎道の写真だったのですが、それはヒッチハイクの経験があればどこか見たことある光景でした。写真の下には "Stay hungry, Stay foolish." という言葉が書かれていたのです。</p>
+    
+    <ul class="info">
+        <li><i class="icon-user"></i> <a href="#">takewick</a></li>
+        <li><i class="icon-heart"></i> <a href="#">いいね！</a></li>
+        <li><i class="icon-inbox"></i> <a href="#">課題の解決方法を投稿</a></li>
+        <li><i class="icon-info-sign"></i> <a href="#">もっと詳しく</a></li>
+    </ul>
+        
+    <div class="solution">
+        <dl>
+            <dt>自分の根性、運命、人生、カルマ、何でもいいから、とにかく信じるのです。歩む道のどこかで点と点がつながると信じれば、自信を持って思うままに生きることができます。たとえ人と違う道を歩んでも、信じることが全てを変えてくれるのです。</dt>
+            <dd>
+                <ul class="info">
+                    <li><i class="icon-user"></i> <a href="#">summerwind</a></li>
+                    <li><i class="icon-heart"></i> <a href="#">いいね！</a></li>
+                </ul>
+            </dd>
+            <dt>未来に先回りして点と点をつなげることはできない。君たちにできるのは過去を振り返ってつなげることだけなんだ。だから点と点がいつか何らかのかたちでつながると信じなければならない。</dt>
+            <dd>
+                <ul class="info">
+                    <li><i class="icon-user"></i> <a href="#">summerwind</a></li>
+                    <li><i class="icon-heart"></i> <a href="#">いいね！</a></li>
+                </ul>
+            </dd>
+            <dt>Steve Jobs、なんで死んでしもうたんや。</dt>
+            <dd>
+                <ul class="info">
+                    <li><i class="icon-user"></i> <a href="#">summerwind</a></li>
+                    <li><i class="icon-heart"></i> <a href="#">いいね！</a></li>
+                </ul>
+            </dd>
+        </dl>
+    </div>
 </div>
 
 <div class="issue">
-<h2>Issue No.3</h2>
-<p>You can't connect the dots looking forward; you can only connect them looking backwards. So you have to trust that the dots will somehow connect in your future. You have to trust in something — your gut, destiny, life, karma, whatever. This approach has never let me down, and it has made all the difference in my life.</p>
-<p>by htakei - <a href="#">Like (21)</a> - <a href="#">Solution (5)</a> - <a href="#">See more</a></p>
-<dl class="solution">
-    <dt>This is a title of solution, Wohooooooooooooooooooooooooooooooo!!</dt>
-    <dd class="info">by mishizaw - <a href="#">Like (2)</a></p>
-    <dt>This is a title of solution, Wohooooooooooooooooooooooooooooooo!!</dt>
-    <dd class="info">by mishizaw - <a href="#">Like (2)</a></p>
-    <dt>This is a title of solution, Wohooooooooooooooooooooooooooooooo!!</dt>
-    <dd class="info">by mishizaw - <a href="#">Like (2)</a></p>
-</dl>
+    <h2>Stay foolish, Stay hungry.</h2>
+    <p>私が若い頃 "The Whole Earth Catalogue 全地球カタログ" というすごい出版物があって、私と同じ世代ではバイブルのように扱われていました。それはステュアート・ブランドという人が、ここからそれほど遠くないメンローパークで制作したもので、彼の詩的なタッチで彩られていました。１９６０年代の終わり頃はパソコンもDTPもない時代ですから、全てタイプライターとハサミとポラロイドカメラで作られていました。それはまるでグーグルのペーパーバック版のようなもので、グーグルが３５年遡って登場したかのような理想的な本で、すごいツールと壮大な概念に溢れかえっていました。</p>
+    <p>スチュアートと彼のチームは ”The Whole Earth Catalogue” を何度か発行しましたが、ひと通りの内容を網羅した時点で最終号を出しました。それは１９７０年代半ばで、私がちょうど君たちの年代だった頃です。最終号の裏表紙は、朝早い田舎道の写真だったのですが、それはヒッチハイクの経験があればどこか見たことある光景でした。写真の下には "Stay hungry, Stay foolish." という言葉が書かれていたのです。</p>
+    
+    <ul class="info">
+        <li><i class="icon-user"></i> <a href="#">takewick</a></li>
+        <li><i class="icon-heart"></i> <a href="#">いいね！</a></li>
+        <li><i class="icon-inbox"></i> <a href="#">課題の解決方法を投稿</a></li>
+        <li><i class="icon-info-sign"></i> <a href="#">もっと詳しく</a></li>
+    </ul>
+        
+    <div class="solution">
+        <dl>
+            <dt>自分の根性、運命、人生、カルマ、何でもいいから、とにかく信じるのです。歩む道のどこかで点と点がつながると信じれば、自信を持って思うままに生きることができます。たとえ人と違う道を歩んでも、信じることが全てを変えてくれるのです。</dt>
+            <dd>
+                <ul class="info">
+                    <li><i class="icon-user"></i> <a href="#">summerwind</a></li>
+                    <li><i class="icon-heart"></i> <a href="#">いいね！</a></li>
+                </ul>
+            </dd>
+            <dt>未来に先回りして点と点をつなげることはできない。君たちにできるのは過去を振り返ってつなげることだけなんだ。だから点と点がいつか何らかのかたちでつながると信じなければならない。</dt>
+            <dd>
+                <ul class="info">
+                    <li><i class="icon-user"></i> <a href="#">summerwind</a></li>
+                    <li><i class="icon-heart"></i> <a href="#">いいね！</a></li>
+                </ul>
+            </dd>
+            <dt>Steve Jobs、なんで死んでしもうたんや。</dt>
+            <dd>
+                <ul class="info">
+                    <li><i class="icon-user"></i> <a href="#">summerwind</a></li>
+                    <li><i class="icon-heart"></i> <a href="#">いいね！</a></li>
+                </ul>
+            </dd>
+        </dl>
+    </div>
 </div>
 
--- ideapocket/fuel/app/views/template.php
+++ ideapocket/fuel/app/views/template.php
@@ -4,154 +4,59 @@
 	<meta charset="utf-8">
 	<title><?php echo $title; ?></title>
 	<?php echo Asset::css('bootstrap.css'); ?>
-	<link href='http://fonts.googleapis.com/css?family=Fredoka+One' rel='stylesheet' type='text/css'>
-	<style>
-    body {
-      padding: 20px 40px;
-      border-top: 8px solid #000;
-    }
-    
-    header {
-        padding: 0 0 20px 0;
-    }
-    
-    h1 {
-        font-family: 'Fredoka One', sans-serif;
-    }
-    
-    #content {
-      margin: 20px 0 0; 
-    }
-
-    .issue {
-        padding: 0 0 30px 0;
-    }
-
-    .issue h2 {
-        position: relative;
-    }
-
-    .issue h2 a {
-        position: absolute;
-        right: 0;
-        font-size: 12px;
-        color: white;
-        background-color: #0069d6;
-        line-height: 1;
-        padding: 7px;
-    }
-
-    .solution {
-        border-bottom: 1px solid #ccc;
-        list-style: none;
-    }
-
-    .solution dt {
-        padding: 8px 0 2px 0;
-        position: relative;
-        border-top: 1px solid #ccc;
-    }
-
-    .solution dt a {
-        position: absolute;
-        right: 0;
-        font-size: 12px;
-        color: white;
-        background-color: #0069d6;
-        line-height: 1;
-        padding: 4px;
-    }
-
-    .solution dd {
-        margin: 0;
-    }
-
-    .solution dd ul {
-        margin: 0;
-        list-style: none;
-    }
-
-    .solution dd ul li {
-        padding: 2px 0 0;
-        border-bottom: 1px solid #ccc;
-    }
-    
-    .rank {
-        padding-bottom: 20px;
-    }
-    
-    .rank h2 {
-        font-size: 15px;
-        border-bottom: 1px solid #ccc;
-    }
-    
-    .rank ol {
-        margin: 0;
-        list-style: none;
-    }
-    
-    .rank ol li {
-        padding: 5px 0;
-        border-bottom: 1px solid #ccc;
-    }
-    
-    .rank ol li span {
-        width: 20px;
-        margin: 0 5px 0 0;
-        text-align: center;
-        display: inline-block;
-    }
-    </style>
+	<?php echo Asset::css('ideapocket.css'); ?>
+	<link href='http://fonts.googleapis.com/css?family=Signika:700' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
-    <div class="row-fluid">
-        <div class="span12">
-            <div id="content" class="row-fluid">
-                <div class="span3">
-                    <header>
-                        <h1><?php echo $title; ?></h1> 
-                        <p>Submit your issue!</p>
-                    </header>
-                    
-                    <form class="well">
-                        <input type="text" placeholder="Issue"><br>
-                        <textarea rows="13" placeholder="Description"></textarea><br>
-                        <button type="submit" class="btn">Submit</button>
-                    </form>
-                    
-                    <div class="rank">
-                        <h2>Like Ranking</h2>
-                        <ol>
-                            <li><span class="badge badge-info">123</span> <a href="#">こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは</a></li>
-                            <li><span class="badge badge-info">18</span> <a href="#">Issue No.2</a></li>
-                            <li><span class="badge badge-info">10</span> <a href="#">Issue No.3</a></li>
-                            <li><span class="badge badge-info">8</span> <a href="#">Issue No.4</a></li>
-                            <li><span class="badge badge-info">3</span> <a href="#">Issue No.5</a></li>
-                        </ol>
-                    </div>
-          
-                    <div class="rank">
-                        <h2>Solution Ranking</h2>
-                        <ol>
-                            <li><span class="badge badge-info">23</span> <a href="#">Issue No.1</a></li>
-                            <li><span class="badge badge-info">18</span> <a href="#">Issue No.2</a></li>
-                            <li><span class="badge badge-info">10</span> <a href="#">Issue No.3</a></li>
-                            <li><span class="badge badge-info">8</span> <a href="#">Issue No.4</a></li>
-                            <li><span class="badge badge-info">3</span> <a href="#">Issue No.5</a></li>
-                        </ol>
-                    </div>
-                </div>
-                <div class="span9"><?php echo $content ?></div>
+    <header>
+        <h1><?php echo $title; ?>*</h1> 
+        <p>Think, Imagine, Hack!</p>
+    </header>
+    
+    <div id="content" class="row-fluid">
+        <div id="main" class="span8">
+            <?php echo $content ?>
+        </div>
+        
+        <div id="aside" class="span4"> 
+            <div id="issue-form">
+                <h2>解決するべき課題</h2>
+                <p>あなたが解決するべきだと思う<strong>課題</strong>を投稿してください。</p>
+                <form class="well">
+                    <p><input type="text" placeholder="タイトル" class="span4"></p>
+                    <p><textarea rows="13" placeholder="具体的な課題"></textarea></p>
+                    <p><button type="submit" class="btn">投稿</button></p>
+                </form>
+            </div>
+            
+            <div class="rank">
+                <h3>人気の課題</h3>
+                <ol>
+                    <li><span class="badge badge-success">123</span> <a href="#">こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは</a></li>
+                    <li><span class="badge badge-success">18</span> <a href="#">Issue No.2</a></li>
+                    <li><span class="badge badge-success">18</span> <a href="#">Issue No.2</a></li>
+                    <li><span class="badge badge-success">18</span> <a href="#">Issue No.2</a></li>
+                    <li><span class="badge badge-success">18</span> <a href="#">Issue No.2</a></li>
+                </ol>
+            </div>
+  
+            <div class="rank">
+                <h3>人気の解決方法</h3>
+                <ol>
+                    <li><span class="badge badge-success">123</span> <a href="#">こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは、こんにちは</a></li>
+                    <li><span class="badge badge-success">18</span> <a href="#">Issue No.2</a></li>
+                    <li><span class="badge badge-success">18</span> <a href="#">Issue No.2</a></li>
+                    <li><span class="badge badge-success">18</span> <a href="#">Issue No.2</a></li>
+                    <li><span class="badge badge-success">18</span> <a href="#">Issue No.2</a></li>
+                </ol>
             </div>
         </div>
     </div>
     
     <footer>
-        <p>
-        Made by &quot;Team Messy&quot;<br>
-        <small>Version: 0.1b</small>
-        </p>
+        <p>Hacked by Team Messy<br>
+        <small>Build 20120421</small></p>
     </footer>
 </body>
 </html>
